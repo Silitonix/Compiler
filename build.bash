475c1f942c54e7d80123fb08c02386cef616642c
Silitonix <silitonix@gmail.com>
2023-08-26 10:53:55 +0330

    fix command argments and added read file to script

--- build.bash
+++ build.bash
@@ -1,11 +1,14 @@
 #!/bin/sh
+IFS=','
 
-echo \
- " const args = '$@'.split()\
- $(cat errors.js | sed "s/ _/ errors/g") \
- $(cat lexer.js | sed "s/ _/ lexer_/g") \
- $(cat parser.js | sed "s/ _/ parser_/g") \
- $(cat emitter.js | sed "s/ _/ emitter_/g") \
- $(cat compiler.js | sed "s/ _/ compiler_/g") \
- " \
-| node
+echo -e \
+" const args = '$*'.split('$IFS');\
+\n const code = \`$([ -f $1 ] && cat $1)\`
+\n $(cat errors.js | sed "s/ _/ errors/g") \
+\n $(cat lexer.js | sed "s/ _/ lexer_/g") \
+\n $(cat parser.js | sed "s/ _/ parser_/g") \
+\n $(cat emitter.js | sed "s/ _/ emitter_/g") \
+\n $(cat compiler.js | sed "s/ _/ compiler_/g") \
+" | node
+
+IFS=' '
