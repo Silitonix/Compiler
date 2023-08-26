634723f0cf67c1332f76c02bbc56b0bf5457bb7e
Silitonix <silitonix@gmail.com>
2023-08-26 10:19:29 +0330

    Make files

---
+++ build.bash
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo \
+ "\
+ $(cat errors.js | sed "s/ _/ errors/g") \
+ $(cat lexer.js | sed "s/ _/ lexer_/g") \
+ $(cat parser.js | sed "s/ _/ parser_/g") \
+ $(cat emitter.js | sed "s/ _/ emitter_/g") \
+ $(cat compiler.js | sed "s/ _/ compiler_/g") \
+ " \
+| node
---
+++ lexer.js
@@ -0,0 +1,10 @@
+let _input = '';
+let _cursor = 0;
+let _col = 0;
+let _row = 0;
+let _char = '';
+
+function lexer () {
+
+  console.log( ' _' );
+}
