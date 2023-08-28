e82d939b173d6546d11d1dd411e52c7b2228dcd6
Silitonix <silitonix@gmail.com>
2023-08-28 11:12:13 +0330

    add comments to builder and re struct the directorys

--- build.sh
+++ build.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
+
+# Store and change the IFS variable
+DefaultIFS=$IFS
 IFS=','
 
+# Read code from a file and if not exist, set code to empty
 code="const code = \`$([ $# -gt 0 ] && [ -f $1 ] && cat $1 || echo '¡')\`;";
+
+# List all arguments and convert them to an array
 args="const args = \`$*\`.split('$IFS');";
 
+# Define file and directory paths
 outputDirectory="build"
-output="build/compiler.js"
+sourceDirectory="src"
+outputScript="$outputDirectory/compiler.js"
 
+# Create build directory if not exist
 mkdir -p $outputDirectory;
 
-echo $code > $output
-echo $args >> $output
-cat console.js >> $output
-cat errors.js >> $output
-cat tokent.js >> $output
-cat token.js >> $output
-cat regex.js >> $output
-cat lexer.js >> $output
-cat parser.js >> $output
-cat emitter.js >> $output
+# Write code to output file
+echo $code > $outputScript
+echo $args >> $outputScript
 
-node $output
+# Concatenating the script to the output file
+cat "$sourceDirectory/console.js" >> $outputScript
+cat "$sourceDirectory/errors.js" >> $outputScript
+cat "$sourceDirectory/tokent.js" >> $outputScript
+cat "$sourceDirectory/token.js" >> $outputScript
+cat "$sourceDirectory/regex.js" >> $outputScript
+cat "$sourceDirectory/lexer.js" >> $outputScript
+cat "$sourceDirectory/parser.js" >> $outputScript
+cat "$sourceDirectory/emitter.js" >> $outputScript
+
+# Run the node js with output
+node $outputScript
+
+# Restore default value of IFS
+IFS=$DefaultIFS
